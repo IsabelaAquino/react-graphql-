5486f5dbe5c59525536381d65f2323b1dd8abbef
Isabela Aquino <isabela.isavzp@gmail.com>
2022-08-08 18:05:07 -0300

    mutations editar e remover

--- graphql-server/src/resolvers/Mutation.js
+++ graphql-server/src/resolvers/Mutation.js
@@ -17,6 +17,37 @@ const Mutation = {
       return user;
 
     },
+
+    editUser(parent, {id, name, email, phone, password, avatar}, { db }, info){
+      const user = {
+        id,
+        name,
+        email,
+        phone,
+        password,
+        avatar
+      }
+
+      for (let i = 0; i < db.users.length; i++) {
+        if (db.users[i].id === user.id) {
+          db.users[i] = user;
+          break;
+        }
+      }
+
+      return user;
+    },
+
+    deleteUser(parent, {id}, { db }, info){
+
+      let newUsers = db.users.filter(function(item) {
+          return item.id != id;
+      });
+
+      db.users = newUsers
+
+      return db.users;
+    },
 };
   
 export default Mutation;
